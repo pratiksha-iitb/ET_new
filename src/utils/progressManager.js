14194a0a26f9c202c75d19ee122d2d2d7e3678dd
Pratiksha <pratikshajjn045@gmail.com>
2026-03-29 18:13:55 +0530

    second_commit

--- src/utils/progressManager.js
+++ src/utils/progressManager.js
@@ -1,10 +1,35 @@
-const KEY = "its_progress";
+// const KEY = "its_progress";
 
+// export function loadProgress(studentId) {
+//   const data = JSON.parse(localStorage.getItem(KEY) || "{}");
+
+//   if (!data[studentId]) {
+//     data[studentId] = {
+//       subtopics: {
+//         1: { score: 0, completed: false },
+//         2: { score: 0, completed: false },
+//         3: { score: 0, completed: false },
+//         4: { score: 0, completed: false },
+//         5: { score: 0, completed: false },
+//       },
+//       overallScore: 0,
+//     };
+//     localStorage.setItem(KEY, JSON.stringify(data));
+//   }
+
+//   return data[studentId];
+// }
+
+// export function saveProgress(studentId, progress) {
+//   const data = JSON.parse(localStorage.getItem(KEY) || "{}");
+//   data[studentId] = progress;
+//   localStorage.setItem(KEY, JSON.stringify(data));
+// }
 export function loadProgress(studentId) {
-  const data = JSON.parse(localStorage.getItem(KEY) || "{}");
+  const data = localStorage.getItem(`progress_${studentId}`);
 
-  if (!data[studentId]) {
-    data[studentId] = {
+  if (!data) {
+    return {
       subtopics: {
         1: { score: 0, completed: false },
         2: { score: 0, completed: false },
@@ -14,14 +39,11 @@ export function loadProgress(studentId) {
       },
       overallScore: 0,
     };
-    localStorage.setItem(KEY, JSON.stringify(data));
   }
 
-  return data[studentId];
+  return JSON.parse(data);
 }
 
 export function saveProgress(studentId, progress) {
-  const data = JSON.parse(localStorage.getItem(KEY) || "{}");
-  data[studentId] = progress;
-  localStorage.setItem(KEY, JSON.stringify(data));
+  localStorage.setItem(`progress_${studentId}`, JSON.stringify(progress));
 }
